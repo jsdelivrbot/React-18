976475f4ddb0fb0710dc97a134427833bb2f1463
Alvaro Mayo <alvaromayov@gmail.com>
2018-03-13 20:44:29 +0000

    Adding functionality to the searchbar
    Adding functionality to the searchbar to make it interact with the video list and items

--- wetube/src/index.js
+++ wetube/src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-import Searchbar from './Searchbar';
+import Searchbar from './components/Searchbar';
+import VideoList from './components/video_list';
+const API_KEY = "AIzaSyDv9LQcJPPtxyidK4b71Yk7-3uCvpMCtf0";
 
-//const API_KEY = "AIzaSyDv9LQcJPPtxyidK4b71Yk7-3uCvpMCtf0";
 
-const App = () => {
-  return(
-    <div>
-      <Searchbar />
-    </div>
 
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {videos: []};
+    YTSearch({key: API_KEY, term: 'surfboard'}, (videos) => {
+      this.setState({videos : videos});
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <Searchbar />
+        <VideoList videos={this.state.videos} />
+      </div>
+
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
